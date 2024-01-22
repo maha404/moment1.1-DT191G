92ef0bc6a89695f208396eeee68bba037d59c45f
maha404 <117388545+maha404@users.noreply.github.com>
2024-01-22 14:36:36 +0100

    Lade till javascript funktioner.

--- razor/wwwroot/js/site.js
+++ razor/wwwroot/js/site.js
@@ -1,4 +1,32 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿
+// Knapp funktion för menyn. 
+let btnContainer = document.getElementById('list');
 
-// Write your JavaScript code.
+let btns = btnContainer.getElementsByClassName('listItem');
+
+for(let i = 0; i < btns.length; i++) {
+    btns[i].addEventListener('click', function () {
+        var current = document.getElementsByClassName("active");
+
+        if (current.length > 0) {
+          current[0].className = current[0].className.replace(" active", "");
+        }
+
+        this.className += " active";
+    });
+}
+
+//Dropdown som finns på Razor undersidan
+let dropdownBtn = document.getElementById("information");
+let content = document.getElementById("textbox");
+
+content.style.display = "none";
+
+dropdownBtn.addEventListener("click", function() {
+    if(content.style.display != "block") {
+        content.style.display = "block";
+    } else {
+        content.style.display = "none";
+    }
+})
+
