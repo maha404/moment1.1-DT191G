4be03b3c6fa815f7b05d68d8dbf6230dd092915f
maha404 <117388545+maha404@users.noreply.github.com>
2024-01-23 12:52:32 +0100

    Tog bort funktionen för meny knapparna.

--- razor/wwwroot/js/site.js
+++ razor/wwwroot/js/site.js
@@ -1,22 +1,4 @@
-﻿
-// Knapp funktion för menyn. 
-let btnContainer = document.getElementById('list');
-
-let btns = btnContainer.getElementsByClassName('listItem');
-
-for(let i = 0; i < btns.length; i++) {
-    btns[i].addEventListener('click', function () {
-        var current = document.getElementsByClassName("active");
-
-        if (current.length > 0) {
-          current[0].className = current[0].className.replace(" active", "");
-        }
-
-        this.className += " active";
-    });
-}
-
-//Dropdown som finns på Razor undersidan
+﻿//Dropdown som finns på Razor undersidan
 let dropdownBtn = document.getElementById("information");
 let content = document.getElementById("textbox");
 
